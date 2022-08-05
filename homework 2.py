4043c71d979e5f40230427f94a4bf31e9b6365ed
Игорь <ziikibox@gmail.com>
2022-08-05 23:53:18 +0300

    Add file

--- homework 2.py
+++ homework 2.py
@@ -1,1 +1,11 @@
-print("Hello, Igor!")
+print("Здравствуй, Евгений, я хочу сыграть с тобой в игру.")
+
+numbers = input("Введи номер своей банковской карты: ")
+
+even_numbers = numbers[1::2]
+
+reverse_numbers_big = numbers[-1::-1]
+
+print(numbers)
+print(even_numbers)
+print(reverse_numbers_big.upper())
