7a800f95287bdbfa61ba035c9ff494cc6262ccb1
Игорь <ziikibox@gmail.com>
2022-08-07 13:15:55 +0300

    Add file

---
+++ homework_3.py
@@ -0,0 +1,33 @@
+igor_age = 32
+
+alex_age = 32
+
+roma_age = 32
+
+print(igor_age == alex_age == roma_age)
+
+print(igor_age is alex_age is roma_age)
+
+lena_age = str(igor_age - 4)
+
+olya_age = str(alex_age - 4)
+
+print(lena_age == olya_age)
+
+print(lena_age is olya_age)
+
+igor_age = [igor_age]
+
+alex_age = [alex_age]
+
+roma_age = [roma_age]
+
+print(igor_age == alex_age == roma_age)
+
+print(igor_age is alex_age is roma_age)
+
+olya_age = lena_age
+
+print(lena_age == olya_age)
+
+print(lena_age is olya_age)
